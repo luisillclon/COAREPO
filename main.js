931b5fc304b690059f9e0952fe34243efc7c58b0
LuisClon <114313768+Luisill4036W@users.noreply.github.com>
2024-02-20 18:28:26 -0500

    Segunda Version final and finally, available for cellphones

---
+++ main.js
@@ -0,0 +1,6 @@
+const appHeight = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+window.addEventListener('resize', appHeight)
+appHeight()
